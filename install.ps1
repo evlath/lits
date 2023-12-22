7f40a0fd93418116b2ead845bea0dc43b9f0ac53
evlath <11215480+evlath@users.noreply.github.com>
2023-12-22 10:12:29 -0700

    Got each type of installer working

--- install.ps1
+++ install.ps1
@@ -1,46 +1,56 @@
-$shortname = ""                                 # NAME OF PROGRAM (e.g. 7zip, Solidworks 2023, etc)
-$verison = ""               
-$installer_path = ""                            # RELATIVE PATH TO INSTALLER (e.g. .\setup.exe, .\windows\setup.msi, \\nasshare2.engr.colostate.edu\software\solidworks\2023\setupad.exe)
-$installer_parameters = ""                      # PARAMETERS FOR INSTALLER
-$is_msi_install = $false
+$shortname = "";
+$verison = "";
+$installer_path = "";
+$installer_argumments = "";
+$is_msi_install = $false;
+# IF YOU COPY FROM AN EXISTING SCRIPT SET THIS TO $true AND is_msi_install to $false
+$install_script = $false;
 
 # Vars for tests
-$installed_dir = ""
-$exe_that_should_exist = ""
+$installed_dir = "";
+$exe_that_should_exist = "";
 
 # INSTALL PROGRAM
 
 ## MSI INSTALL
 if ($is_msi_install) {
-    msiexec /i $installer_path /qn
+    Start-Process msiexec "/i $($installer_path) $($installer_argumments)" -Wait;
 }
 
 ## NON MSI INSTALL
 else {
-    $installer_path
+    if ($install_script) {
+
+        ### PASTE SCRIPT HERE IF COPYING OVER!
+
+    } else {
+        Start-Process -FilePath $installer_path -ArgumentList "$($installer_argumments)" -Wait;
+    }
+    
 }
 
 
 
 # TESTS
+# The way the below works is when you throw an exception, powershell sets the exit code as 1, we will later have MDT catch these
 ## DIRECTORY EXISTANCE TEST
 
 if (Test-Path -Path $installed_dir) {
     # TODO add log
-    "Path exists!"
+    "Path exists!";
 } else {
     # TODO add log
     "Path doesn't exist."
-    throw "$($shortname) failed on DIRECTORY TEST. $($installed_dir) does not exist"
+    throw "$($shortname) failed on DIRECTORY TEST. $($installed_dir) does not exist";
 }
 
 ## FILE EXISTANCE TEST
 
 if (Test-Path -Path $exe_that_should_exist) {
     # TODO add log
-    "Path exists!"
+    "Path exists!";
 } else {
     # TODO add log
     "Path doesn't exist."
-    throw "$($shortname) failed on EXE TEST. $($exe_that_should_exist) does not exist"
+    throw "$($shortname) failed on EXE TEST. $($exe_that_should_exist) does not exist";
 }
