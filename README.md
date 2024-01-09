af9797298270b36c174a0eef4ebbd70330ea99f4
evlath <11215480+evlath@users.noreply.github.com>
2024-01-09 13:48:07 -0700

    rewrite

--- README.md
+++ README.md
@@ -1,18 +1,9 @@
-# etsluis
-(LUIS) Labs Unified Install Script
+# LITS
+(LITS) Labs Install Test Script
 
 ## Description
-LUIS is a template silent install script for use in the ETS-Labs MDT Infrastructure.
+LITS is a script that will be ran at the end of an image to verify that a lab machine has all software installed to it
 
-## Why do we need it?
-LUIS will aim to verfy the successful install of a program by use of simple file/directory checks to see if a program was installed (or at least the expected files were)
-
-## How will it work?
-The first iterations of luis will check for the existance of a file. In later versions, version validation and file integraty will be verified as well.
-
-## What will it do?
-### Current
-- Install program
-
-### Planned and in progress
-- Check file existance
+## Current Tests
+- Test the existance of a directory that would imply that something got installed
+- Test the existance of a file that would imply that something got installed
---
+++ test.ps1
@@ -0,0 +1,61 @@
+# Version: 0.2
+# FOR TESTING INSTALLS ONLY!
+# THIS SCRIPT IS NOT READY FOR USE AS AN INSTALL SCRIPT
+
+param(
+     [string]$csvFile = ".\software.csv",
+     [switch]$exitOnFail = $false,
+     [string]$logPath = "\\labs-mdt\log$"
+ )
+
+function TestAppInstall {
+
+    param (
+        $ShortName,
+        $Version,
+        $Directory,
+        $File
+    )
+
+    $date = Get-Date -Format "yyyy-MM-dd HH:mm:ss";
+
+    # TEST SCRIPT
+    $failed = $false;
+    ## DIRECTORY EXISTANCE TEST
+
+    if (Test-Path -Path $Directory) {
+        "";
+    } else {
+        "[$date] $ShortName $Version FAILED on DIRECTORY TEST. $Directory does not exist" | Out-File -FilePath "$logPath\$env:computername\luislog.log" -Encoding "Default" -Append;
+        if ($exitOnFail) {
+            throw "";
+        } 
+        $failed = $true;
+    }
+
+    ## FILE EXISTANCE TEST
+
+    if (Test-Path -Path $File) {
+        "";
+    } else {
+        "[$date] $ShortName $Version FAILED on FILE EXISTANCE TEST. $File does not exist" | Out-File -FilePath "$logPath\$env:computername\luislog.log" -Encoding "Default" -Append;
+        if ($exitOnFail) {
+            throw "";
+        }
+        $failed = $true;
+    }
+
+
+    if ($failed) {
+        throw "";
+    }
+        
+    ## WRITE SUCCESS TO LOG
+    "[$date] $ShortName $Version PASSED" | Out-File -FilePath "$logPath\$env:computername\luislog.log" -Encoding "Default" -Append;
+}
+
+
+
+Import-Csv -Path $csvFile | foreach {
+    TestAppInstall $_.ShortName $_.Version $_.Directory $_.File;
+}
