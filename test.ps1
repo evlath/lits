b6473bb3c80debc0828a572abcaed962cdd80ab7
evlath <11215480+evlath@users.noreply.github.com>
2024-01-11 17:00:28 -0700

    hello

--- test.ps1
+++ test.ps1
@@ -6,34 +6,36 @@ $csvfile = "./software.csv"
 $computers = Get-ADComputer -Filter * -SearchBase $ouPath
 $filedate = Get-Date -Format "yyyy-MM-dd-HHmmss";
 
-$logdir = "."
-$globallogpath = "$logdir\globallits.log"
-$computerlogname = "lits"
+$logdir = ".\Logs"
+$globallogpath = "$logdir\$filedate-globallits.log"
+$computerlogname = "lits-$filedate"
 
 foreach ($computer in $computers) {
+
+    $cn = $computer.Name;
 
     $total = 0;
     $fail = 0;
 
-    Write-Host "Starting Test on: $($computer.Name)"
-    $logpath = "$logdir\$($computer.Name)-$computerlogname.log"
+    Write-Host "Starting Test on: $($cn)"
+    $logpath = "$logdir\$($cn)-$computerlogname.log"
     
     $date = Get-Date -Format "yyyy-MM-dd HH:mm:ss";
-    "[$date] $($computer.Name) STARTED" | Out-File -FilePath $logpath -Encoding "Default" -Append;
+    "[$date] $($cn) STARTED" | Out-File -FilePath $logpath -Encoding "Default" -Append;
 
     Import-Csv -Path $csvFile | ForEach-Object {
         #$_.ShortName $_.Version $_.Directory $_.File
         $date = Get-Date -Format "yyyy-MM-dd HH:mm:ss";
-        $cshare_Directory = "\\$($computer.Name)\$($_.Directory.replace(':','$'))"
-        $cshare_File = "\\$($computer.Name)\$($_.File.replace(':','$'))"
+        $cshare_Directory = "\\$($cn)\$($_.Directory.replace(':','$'))"
+        $cshare_File = "\\$($cn)\$($_.File.replace(':','$'))"
         $failed = $false;
 
         if (!(Test-Path -Path $cshare_Directory)) {
-            "[$date] FAILED $($_.ShortName) $($_.Version)  on DIRECTORY TEST. $($_.Directory) does not exist ON $($computer.Name)" | Out-File -FilePath $logpath -Encoding "Default" -Append;
+            "[$date] FAILED $($_.ShortName) $($_.Version)  on DIRECTORY TEST. $($_.Directory) does not exist ON $($cn)" | Out-File -FilePath $logpath -Encoding "Default" -Append;
             $failed = $true;
         }
         if (!(Test-Path -Path $cshare_File)) {
-            "[$date] FAILED $($_.ShortName) $($_.Version)  on FILE TEST. $($_.File) does not exist ON $($computer.Name)" | Out-File -FilePath $logpath -Encoding "Default" -Append;
+            "[$date] FAILED $($_.ShortName) $($_.Version)  on FILE TEST. $($_.File) does not exist ON $($cn)" | Out-File -FilePath $logpath -Encoding "Default" -Append;
             $failed = $true;
         }
         if ($failed) {
@@ -47,15 +49,15 @@ foreach ($computer in $computers) {
     $date = Get-Date -Format "yyyy-MM-dd HH:mm:ss";
     $success = $total - $fail;
 
-    "[$date] $($computer.Name) had $fail failures" | Out-File -FilePath $logpath -Encoding "Default" -Append;
-    "[$date] $($computer.Name) had $success successes" | Out-File -FilePath $logpath -Encoding "Default" -Append;
-    "[$date] $($computer.Name) had $total total tested apps" | Out-File -FilePath $logpath -Encoding "Default" -Append;
-    "[$date] $($computer.Name) ENDED" | Out-File -FilePath $logpath -Encoding "Default" -Append;
+    "[$date] $($cn) had $fail failures" | Out-File -FilePath $logpath -Encoding "Default" -Append;
+    "[$date] $($cn) had $success successes" | Out-File -FilePath $logpath -Encoding "Default" -Append;
+    "[$date] $($cn) had $total total tested apps" | Out-File -FilePath $logpath -Encoding "Default" -Append;
+    "[$date] $($cn) ENDED" | Out-File -FilePath $logpath -Encoding "Default" -Append;
 
     if ($fail -gt 0) {
-        "[$date] $($computer.Name) FAILED with $fail failures" | Out-File -FilePath $globallogpath -Encoding "Default" -Append;
+        "[$date] $($cn) FAILED with $fail failures" | Out-File -FilePath $globallogpath -Encoding "Default" -Append;
     } else {
-        "[$date] $($computer.Name) PASSED with $success successes" | Out-File -FilePath $globallogpath -Encoding "Default" -Append;
+        "[$date] $($cn) PASSED with $success successes" | Out-File -FilePath $globallogpath -Encoding "Default" -Append;
     }
 
 }
